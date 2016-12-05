30f173292c82d7606a8a42df098b996f7ff3cc1a
Bruno Jouhier <bjouhier@gmail.com>
2016-12-05 18:49:28 +0100

    wrap only once

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import { run } from 'f-promise';
 export function setup() {
     const glob = global as any;
     const it = glob.it;
+    if (it.wrapped) return;
     glob.it = function (name: string, fn: () => void) {
         return it(name, function (done: MochaDone) {
             run(() => fn()).then(done, done);
         });
     }
+    glob.it.wrapped = true;
 }
