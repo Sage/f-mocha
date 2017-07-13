4b5b3c5cb891d1b1af5d105587233c7a60300666
Bruno Jouhier <bjouhier@gmail.com>
2017-07-13 21:21:01 +0200

    first tslint pass ok

--- src/index.ts
+++ src/index.ts
@@ -4,56 +4,56 @@ import { IHookCallbackContext } from 'mocha';
 export type MochaBody = (this: IHookCallbackContext) => void;
 
 function wrapWithRun(body: MochaBody) {
-    return function (this: IHookCallbackContext, done: MochaDone) {
-        function doneErr(err: any) {
-            if (err && err instanceof Error) {
-                done(err);
-            } else {
-                done();
-            }
-        }
-        // check done is called only when declared (normal mocha behaviour)
-        if (body.length === 0) {
-            run(() => {
-                return body.call(this);
-            }).then(doneErr, done);
-        } else {
-            run(() => {
-                return body.call(this, doneErr);
-            });
-        }
-    };
+	return function (this: IHookCallbackContext, done: MochaDone) {
+		function doneErr(err: any) {
+			if (err && err instanceof Error) {
+				done(err);
+			} else {
+				done();
+			}
+		}
+		// check done is called only when declared (normal mocha behaviour)
+		if (body.length === 0) {
+			run(() => {
+				return body.call(this);
+			}).then(doneErr, done);
+		} else {
+			run(() => {
+				return body.call(this, doneErr);
+			});
+		}
+	};
 }
 
 function overrideFn(fn: Function) {
-    return function (name: string | MochaBody, body?: MochaBody) {
-        if (!body) {
-            if (typeof name === 'string') {
-                return fn(name);
-            } else {
-                return fn(wrapWithRun(name));
-            }
-        }
-        return fn(name, wrapWithRun(body));
-    }
+	return function (name: string | MochaBody, body?: MochaBody) {
+		if (!body) {
+			if (typeof name === 'string') {
+				return fn(name);
+			} else {
+				return fn(wrapWithRun(name));
+			}
+		}
+		return fn(name, wrapWithRun(body));
+	};
 }
 
 export function setup() {
-    function patchFn(fnName: string, subNames?: string[]) {
-        subNames = subNames || [];
-        const _fn = glob[fnName];
-        if (_fn.wrapped) return;
-        glob[fnName] = overrideFn(_fn);
-        subNames.forEach((subFnName) => {
-            glob[fnName][subFnName] = overrideFn(_fn[subFnName]);
-        });
-        glob[fnName].wrapped = true;
-    }
+	function patchFn(fnName: string, subNames?: string[]) {
+		subNames = subNames || [];
+		const _fn = glob[fnName];
+		if (_fn.wrapped) return;
+		glob[fnName] = overrideFn(_fn);
+		subNames.forEach(subFnName => {
+			glob[fnName][subFnName] = overrideFn(_fn[subFnName]);
+		});
+		glob[fnName].wrapped = true;
+	}
 
-    const glob = global as any;
-    patchFn('it', ['only', 'skip']);
-    patchFn('before');
-    patchFn('beforeEach');
-    patchFn('after');
-    patchFn('afterEach');
+	const glob = global as any;
+	patchFn('it', ['only', 'skip']);
+	patchFn('before');
+	patchFn('beforeEach');
+	patchFn('after');
+	patchFn('afterEach');
 }
--- test/basic-test.ts
+++ test/basic-test.ts
@@ -4,139 +4,139 @@ import { setup } from '..';
 
 setup();
 
-describe("basic test", () => {
-    it('sync works', () => {
-        assert(true, 'got true');
-    });
-    it('async works like sync', () => {
-        assert(true, 'got true before wait');
-        wait(cb => setTimeout(cb, 0));
-        assert(true, 'got true after wait');
-    });
-    it.skip('skip', () => {
-        assert(true, "not executed because of skip");
-    });
+describe('basic test', () => {
+	it('sync works', () => {
+		assert(true, 'got true');
+	});
+	it('async works like sync', () => {
+		assert(true, 'got true before wait');
+		wait(cb => setTimeout(cb, 0));
+		assert(true, 'got true after wait');
+	});
+	it.skip('skip', () => {
+		assert(true, 'not executed because of skip');
+	});
 });
 
-describe("basic test on before", () => {
-    describe("sync test", () => {
-        let expected: boolean;
-        before('init expected value', () => {
-            expected = true;
-        });
+describe('basic test on before', () => {
+	describe('sync test', () => {
+		let expected: boolean;
+		before('init expected value', () => {
+			expected = true;
+		});
 
-        it('check expected value set in before', () => {
-            assert(expected, 'got expected value set in before');
-            expected = false;
-        });
+		it('check expected value set in before', () => {
+			assert(expected, 'got expected value set in before');
+			expected = false;
+		});
 
-        it('check expected value set in before but changed in previous it', () => {
-            assert(!expected, 'got expected value set in before');
-        });
-    });
-    describe("async test", () => {
-        let expected: boolean;
-        before('init expected value', () => {
-            assert(true, 'got false before wait');
-            wait(cb => setTimeout(cb, 0));
-            assert(true, 'got true after wait');
-            expected = true;
-        });
+		it('check expected value set in before but changed in previous it', () => {
+			assert(!expected, 'got expected value set in before');
+		});
+	});
+	describe('async test', () => {
+		let expected: boolean;
+		before('init expected value', () => {
+			assert(true, 'got false before wait');
+			wait(cb => setTimeout(cb, 0));
+			assert(true, 'got true after wait');
+			expected = true;
+		});
 
-        it('check expected value set in before', () => {
-            assert(expected, 'got expected value set in before');
-            expected = false;
-        });
+		it('check expected value set in before', () => {
+			assert(expected, 'got expected value set in before');
+			expected = false;
+		});
 
-        it('check expected value set in before but changed in previous it', () => {
-            assert(!expected, 'got expected value set in before');
-        });
-    });
+		it('check expected value set in before but changed in previous it', () => {
+			assert(!expected, 'got expected value set in before');
+		});
+	});
 });
 
-describe("basic test on beforeEach", () => {
-    describe("sync test", () => {
-        let expected: boolean;
-        beforeEach('init expected value', () => {
-            expected = true;
-        });
+describe('basic test on beforeEach', () => {
+	describe('sync test', () => {
+		let expected: boolean;
+		beforeEach('init expected value', () => {
+			expected = true;
+		});
 
-        it('check expected value set in beforeEach', () => {
-            assert(expected, 'got expected value set in before');
-            expected = false;
-        });
+		it('check expected value set in beforeEach', () => {
+			assert(expected, 'got expected value set in before');
+			expected = false;
+		});
 
-        it('check expected value set in beforeEach but changed in previous it', () => {
-            assert(expected, 'got expected value set in before');
-        });
-    });
-    describe("async test", () => {
-        let expected: boolean;
-        beforeEach('init expected value', () => {
-            assert(true, 'got false before wait');
-            wait(cb => setTimeout(cb, 0));
-            assert(true, 'got true after wait');
-            expected = true;
-        });
+		it('check expected value set in beforeEach but changed in previous it', () => {
+			assert(expected, 'got expected value set in before');
+		});
+	});
+	describe('async test', () => {
+		let expected: boolean;
+		beforeEach('init expected value', () => {
+			assert(true, 'got false before wait');
+			wait(cb => setTimeout(cb, 0));
+			assert(true, 'got true after wait');
+			expected = true;
+		});
 
-        it('check expected value set in beforeEach', () => {
-            assert(expected, 'got expected value set in before');
-            expected = false;
-        });
+		it('check expected value set in beforeEach', () => {
+			assert(expected, 'got expected value set in before');
+			expected = false;
+		});
 
-        it('check expected value set in beforeEach but changed in previous it', () => {
-            assert(expected, 'got expected value set in before');
-        });
-    });
+		it('check expected value set in beforeEach but changed in previous it', () => {
+			assert(expected, 'got expected value set in before');
+		});
+	});
 
-    describe("async test without string prefix", () => {
-        let expected: boolean;
-        beforeEach(function () {
-            this.timeout(20000);
-            assert(true, 'got false before wait');
-            wait(cb => setTimeout(cb, 0));
-            assert(true, 'got true after wait');
-            expected = true;
-        });
+	describe('async test without string prefix', () => {
+		let expected: boolean;
+		beforeEach(function () {
+			this.timeout(20000);
+			assert(true, 'got false before wait');
+			wait(cb => setTimeout(cb, 0));
+			assert(true, 'got true after wait');
+			expected = true;
+		});
 
-        it('check expected value set in beforeEach', () => {
-            assert(expected, 'got expected value set in before');
-            expected = false;
-        });
+		it('check expected value set in beforeEach', () => {
+			assert(expected, 'got expected value set in before');
+			expected = false;
+		});
 
-        it('check expected value set in beforeEach but changed in previous it', () => {
-            assert(expected, 'got expected value set in before');
-        });
-    });
+		it('check expected value set in beforeEach but changed in previous it', () => {
+			assert(expected, 'got expected value set in before');
+		});
+	});
 
-    describe("async test with done parameter called too soon", () => {
-        let expected: boolean;
-        beforeEach(function (done) {
-            done();
-            wait(() => setTimeout(() => {
-                expected = true;
-            }, 1000));
-        });
+	describe('async test with done parameter called too soon', () => {
+		let expected: boolean;
+		beforeEach(function (done) {
+			done();
+			wait(() => setTimeout(() => {
+				expected = true;
+			}, 1000));
+		});
 
-        it('check expected value set in beforeEach', (done) => {
-            assert(!expected, 'got expected value set in before');
-            done();
-        });
-    });
+		it('check expected value set in beforeEach', done => {
+			assert(!expected, 'got expected value set in before');
+			done();
+		});
+	});
 
-    describe("async test with done parameter called in time", () => {
-        let expected: boolean;
-        beforeEach(function (done) {
-            assert(true, 'got true after wait');
+	describe('async test with done parameter called in time', () => {
+		let expected: boolean;
+		beforeEach(function (done) {
+			assert(true, 'got true after wait');
 
-            setTimeout(() => {
-                expected = true;
-                done();
-            }, 1000);
-        });
+			setTimeout(() => {
+				expected = true;
+				done();
+			}, 1000);
+		});
 
-        it('check expected value set in beforeEach', () => {
-            assert(expected, 'got expected value set in before');
-        });
-    });
+		it('check expected value set in beforeEach', () => {
+			assert(expected, 'got expected value set in before');
+		});
+	});
 });
